4b51401ea372db06d4313bc400717df0d7134475
alex.frai <alex.fraioli@epitech.eu>
2025-02-27 14:45:22 +0100

    files directory

--- flutter_trell_app/lib/main.dart
+++ flutter_trell_app/lib/main.dart
@@ -50,7 +50,7 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState(); 
 }
 
 class _MyHomePageState extends State<MyHomePage> {
